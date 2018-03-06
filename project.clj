fd80aefe21921ff059d5e0932f104e6c76d72fba
Daniel Schruhl <danielschruhl@gmail.com>
2018-03-06 12:34:59 +0100

    add some more details to project.clj

--- project.clj
+++ project.clj
@@ -1,21 +1,26 @@
 (defproject com.github.discoverAI/snake "0.1.0-SNAPSHOT"
+  :description "A Clojure/ClojureScript Snake game for your browser"
+  :url "https://github.com/DiscoverAI/snake"
+  :license {:name "MIT"}
+  :scm {:name "git"
+        :url  "https://github.com/DiscoverAI/snake"}
+
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.126"]
                  [reagent "0.7.0"]
                  [re-frame "0.10.5"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]]
-
   :min-lein-version "2.5.3"
-
   :source-paths ["src/clj"]
-
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-
   :figwheel {:css-dirs ["resources/public/css"]}
 
+  :lein-release {:deploy-via :clojars}
+
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.9"]]
-                   :plugins      [[lein-figwheel "0.5.13"]]}}
+                   :plugins      [[lein-figwheel "0.5.13"]
+                                  [lein-release/lein-release "1.0.9"]]}}
 
   :cljsbuild {:builds [{:id           "dev"
                         :source-paths ["src/cljs"]
