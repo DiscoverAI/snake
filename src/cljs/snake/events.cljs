7c16302b3ace8c7be9d5f4319c0e8b9ac7cf6bb9
Falco Winkler <falcomail95@yahoo.de>
2018-03-07 17:17:17 +0000

    Revert "Emitting tick event every second"
    This reverts commit ae82f8103a5d50d814c25e5e242fca7f2ee9e572.

--- src/cljs/snake/events.cljs
+++ src/cljs/snake/events.cljs
@@ -1,17 +1,8 @@
 (ns snake.events
   (:require [re-frame.core :as re-frame]
-            [snake.db :as db]
-            [snake.handlers :as handlers]))
+            [snake.db :as db]))
 
 (re-frame/reg-event-db
  ::initialize-db
  (fn  [_ _]
    db/default-db))
-
-
-(defn dispatch-tick
-  []
-  (let [now (js/Date.)]
-    (re-frame/dispatch [:tick now])))
-
-(defonce do-timer (js/setInterval dispatch-tick 1000))
