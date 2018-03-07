964afbdf22cc36309fea9e17f8de1d2f84b7d4b0
Falco Winkler <falcomail95@yahoo.de>
2018-03-07 17:24:53 +0000

    Redo accidental commit to master

--- src/cljs/snake/events.cljs
+++ src/cljs/snake/events.cljs
@@ -1,8 +1,17 @@
 (ns snake.events
   (:require [re-frame.core :as re-frame]
-            [snake.db :as db]))
+            [snake.db :as db]
+            [snake.handlers :as handlers]))
 
 (re-frame/reg-event-db
- ::initialize-db
- (fn  [_ _]
-   db/default-db))
+  ::initialize-db
+  (fn  [_ _]
+    db/default-db))
+
+
+(defn dispatch-tick
+  []
+  (let [now (js/Date.)]
+    (re-frame/dispatch [:tick now])))
+
+(defonce do-timer (js/setInterval dispatch-tick 1000))
