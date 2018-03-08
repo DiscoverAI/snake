e205aad9196c4983eea0960801e0197f49c43f3c
Falco Winkler <falcomail95@yahoo.de>
2018-03-08 11:51:33 +0000

    Movement is a change vector

--- src/clj/snake/state.clj
+++ src/clj/snake/state.clj
@@ -5,6 +5,6 @@
   {:field-width 10
    :field-height 10
    :snake [[5,3],[5,2], [5,1]] ; x,y coordinates for the snake. First is head, last is tail.
-   :movement "right"
+   :movement [0, 1]
    :speed 1.0
    })
