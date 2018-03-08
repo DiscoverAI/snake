aee1e097bf74c935ef8ff42ef05713e199859b0e
Falco Winkler <falcomail95@yahoo.de>
2018-03-08 13:31:31 +0000

    Change board data structure to be more readable

--- src/clj/snake/state.clj
+++ src/clj/snake/state.clj
@@ -3,6 +3,9 @@
 
 (def init-state
   {:board  [5 5]
-   :tokens {:snake {:position  [[5 3] [5 2] [5 1]]
-                    :direction [0 1]
+   :tokens {:snake {
+                    ;[x y] coordinates for the snake
+                    ; first list item is the head, last is tail
+                    :position  [[5 3] [5 2] [5 1]]
+                    :direction [0 1] ; change vector to [x y] position
                     :speed     1.0}}})
