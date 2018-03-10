5e7fd8aa611bf376fdd30c30708f4a7ce9fdfb17
Falco Winkler <falcomail95@yahoo.de>
2018-03-10 19:06:54 +0000

    small test for initialize-db event

---
+++ test/cljs/com/github/discoverAI/snake/events_test.cljs
@@ -0,0 +1,12 @@
+(ns com.github.discoverAI.snake.events-test
+  (:require [cljs.test :refer-macros [deftest is testing]]
+            [com.github.discoverAI.snake.events :as events]))
+
+(deftest init-test
+  (testing "if the initialize event is correctly handled and returnes the (for now static) state map"
+    (is (= db/default-db
+           (events/handle-init-db {} :snake.events/initialize-db)
+           )
+        )
+    )
+  )
