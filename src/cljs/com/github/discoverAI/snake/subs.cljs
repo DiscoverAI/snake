d238dafdc2ca401732b942ef345646d6290873e9
Falco Winkler <falcomail95@yahoo.de>
2018-03-10 19:38:01 +0000

    subscribing to board and snake position, experimenting with views

--- src/cljs/com/github/discoverAI/snake/subs.cljs
+++ src/cljs/com/github/discoverAI/snake/subs.cljs
@@ -2,6 +2,18 @@
   (:require [re-frame.core :as re-frame]))
 
 (re-frame/reg-sub
- ::name
- (fn [db]
-   (:name db)))
+  ::name
+  (fn [db]
+    (:name db)))
+
+(defn get-board [db] (:board db))
+
+(re-frame/reg-sub
+  ::board
+  get-board)
+
+(defn get-snake-position [db] (:position (:snake (:tokens db))))
+
+(re-frame/reg-sub
+  ::snake-position
+  get-snake-position)
--- test/cljs/com/github/discoverAI/snake/events_test.cljs
+++ test/cljs/com/github/discoverAI/snake/events_test.cljs
@@ -1,6 +1,7 @@
 (ns com.github.discoverAI.snake.events-test
   (:require [cljs.test :refer-macros [deftest is testing]]
-            [com.github.discoverAI.snake.events :as events]))
+            [com.github.discoverAI.snake.events :as events]
+            [com.github.discoverAI.snake.db :as db]))
 
 (deftest init-test
   (testing "if the initialize event is correctly handled and returnes the (for now static) state map"
