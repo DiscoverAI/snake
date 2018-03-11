7b1ea7ccaae00668f9a0b51bdfd0ee3c05e389e0
Daniel Schruhl <danielschruhl@gmail.com>
2018-03-11 14:33:43 +0100

    add event for new game button

--- src/cljs/com/github/discoverAI/snake/views.cljs
+++ src/cljs/com/github/discoverAI/snake/views.cljs
@@ -1,13 +1,18 @@
 (ns com.github.discoverAI.snake.views
   (:require [re-frame.core :refer [subscribe dispatch]]
             [com.github.discoverAI.snake.subs :as subs]
-            [com.github.discoverAI.snake.db :as db]))
+            [com.github.discoverAI.snake.db :as db]
+            [com.github.discoverAI.snake.events :as events]))
+
+(defn board-grid []
+  [:span "asd"])
 
 (defn board []
   [:section
    (let [current-state @(subscribe [::subs/current-state])]
      (cond
-       (= db/not-started current-state) [:h2 "Press \"New Game\" button to start"]))])
+       (= db/not-started current-state) [:h2 "Press \"New Game\" button to start"]
+       (= db/started current-state) [board-grid]))])
 
 (defn info-panel []
   [:header
@@ -19,7 +24,9 @@
   [:footer
    (let [current-state @(subscribe [::subs/current-state])]
      (cond
-       (= db/not-started current-state) [:a {:class "btn"} "New Game"]))])
+       (= db/not-started current-state) [:a {:class "btn" :on-click #(dispatch [::events/start-game])} "New Game"]
+       (= db/started current-state) [:span "Use your keyboard arrows to navigate"]
+       :else [:span ""]))])
 
 (defn base-template []
   [:div {:class "wrapper"}
