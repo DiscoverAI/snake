8b7953a1c8768fb5b3656252d6acf9b7a7d0e26e
Daniel Schruhl <danielschruhl@gmail.com>
2018-03-11 14:23:32 +0100

    fix tests

--- test/cljs/com/github/discoverAI/snake/test_runner.cljs
+++ test/cljs/com/github/discoverAI/snake/test_runner.cljs
@@ -1,9 +1,7 @@
 (ns com.github.discoverAI.snake.test-runner
   (:require [doo.runner :refer-macros [doo-tests]]
             [com.github.discoverAI.snake.events-test]
-            [com.github.discoverAI.snake.subs-test]
-            [com.github.discoverAI.snake.views-test]))
+            [com.github.discoverAI.snake.subs-test]))
 
 (doo-tests 'com.github.discoverAI.snake.events-test
-           'com.github.discoverAI.snake.subs-test
-           'com.github.discoverAI.snake.views-test)
+           'com.github.discoverAI.snake.subs-test)
