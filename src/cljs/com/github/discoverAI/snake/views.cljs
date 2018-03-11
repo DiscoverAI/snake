e74db5820e32fdc8549aec0248bbc611cbec2267
Falco Winkler <falcomail95@yahoo.de>
2018-03-11 11:25:48 +0000

    Revert "Refactor a bit"
    This reverts commit ca922f2ed1bb7ee5797272cb906f7ec32eab3fc9.

--- src/cljs/com/github/discoverAI/snake/views.cljs
+++ src/cljs/com/github/discoverAI/snake/views.cljs
@@ -2,53 +2,42 @@
   (:require [re-frame.core :refer [subscribe dispatch]]
             [com.github.discoverAI.snake.subs :as subs]))
 
-(def container-view
-  [:table.container])
+(def table [:table.container])
 
-(def container-body
-  [:tbody])
+(defn pos-is-snake [snake pos]
+  (boolean ((into #{} snake) pos)))
 
-(defn row-container
-  [key]
-  [:tr {:key key}])
-
-(defn pos-is-snake
-  [snake pos]
-  (boolean ((into #{} snake) pos))
-  )
-
-(defn cell-item-for
-  [current-pos snake]
+(defn cell-item-for [current-pos snake]
   (cond
     (pos-is-snake snake current-pos)
     [:td.snake]
     :else
-    [:td.cell]
-    )
-  )
+    [:td.cell]))
 
-(defn create-row
-  [width y snake]
-  (into (row-container y)
-        (for [x (range width) :let [current-pos [x y]]]
-                          (cell-item-for current-pos snake))
-                      ))
+(defn create-row [width y snake]
+  (into [:tr {:key y}]
+        (for [x (range width)
+              :let [current-pos [x y]]]
+          (cell-item-for current-pos snake))))
 
-(defn render-board
-  [width height snake]
-  (let [cells (vec (for [y (range height)]
-                (create-row width y snake)))]
-    (conj container-view (into container-body cells))
-  ))
+(defn render-board [width height snake]
+  (let [cells (for [y (range height)]
+                (create-row width y snake))]
+    (conj table [:tbody cells])))
 
-(defn board
-  []
-  (let [[grid-width grid-height] @(subscribe [:game-board-bounds])
-        snake-positions @(subscribe [:snake-position])]
-    (render-board grid-width grid-height snake-positions)
-    )
-  )
+(defn board []
+  (let [[grid-width grid-height] @(subscribe [::subs/game-board])
+        snake (subscribe [::subs/snake])]
+    (render-board grid-width grid-height snake)))
 
-(defn main-panel
-  []
-  [board])
+(defn info-panel []
+  [:header "Snake"])
+
+(defn hints []
+  [:footer "footer"])
+
+(defn base-template []
+  [:div {:class "wrapper"}
+   [info-panel]
+   #_[board]
+   [hints]])
--- test/cljs/com/github/discoverAI/snake/views_test.cljs
+++ test/cljs/com/github/discoverAI/snake/views_test.cljs
@@ -2,47 +2,34 @@
   (:require [cljs.test :refer-macros [deftest is testing]]
             [com.github.discoverAI.snake.views :as views]))
 
-
-(deftest test-pos-is-snake-positive
+(deftest test-pos-snake
   (testing "If a for a position it is returned true if it is within the snake"
-    (is (= true (views/pos-is-snake [[3 5] [2 5] [1 5]] [1 5])
-           ))))
+    (is (= true
+           (views/pos-is-snake [[3 5] [2 5] [1 5]] [1 5]))))
 
-(deftest test-pos-is-snake-negative
   (testing "If a for a position it is returned true if it is not within the snake"
-    (is (= false (views/pos-is-snake [[3 5] [2 5] [1 5]] [1 42])
-           ))))
-
+    (is (= false
+           (views/pos-is-snake [[3 5] [2 5] [1 5]] [1 42])))))
 
 (deftest test-cell-class-for
   (testing "If the correct cell is returned for the position"
-    (is (= [:td.snake] (views/cell-item-for [1 2] [[1 2]])
-          ))
-    (is (= [:td.cell] (views/cell-item-for [1 3] [[1 2]])
-           ))
-    ))
+    (is (= [:td.snake]
+           (views/cell-item-for [1 2] [[1 2]])))
+
+    (is (= [:td.cell]
+           (views/cell-item-for [1 3] [[1 2]])))))
 
 (deftest create-row-test
   (testing "If a row of cells is created correctly"
-    (let [row-index 0
-          width 4
-          snake [[0 0] [1 0] [2 0]]]
-    (is (= (into (views/row-container row-index) [[:td.snake] [:td.snake] [:td.snake] [:td.cell]])
-           (views/create-row width row-index snake)))
-    )))
+    (is (= [:tr {:key 0} [:td.snake] [:td.snake] [:td.snake] [:td.cell]]
+           (views/create-row 4 0 [[0 0] [1 0] [2 0]])))))
 
 (deftest render-board-test
   (testing "If board is rendered correctly"
     (is (= (views/render-board 2 2 [[1 0]])
-           (into views/container-view [(into views/container-body [
-                                        (into (views/row-container 0) [[:td.cell] [:td.snake]])
-                                           (into (views/row-container 1) [[:td.cell] [:td.cell]])])]))))
-  )
+           (into views/table [[:tbody '([:tr {:key 0} [:td.cell] [:td.snake]]
+                                         [:tr {:key 1} [:td.cell] [:td.cell]])]])))
 
-(deftest render-board-test2
-  (testing "If board is rendered correctly"
     (is (= (views/render-board 2 2 [[0 1] [1 1]])
-           (into views/container-view [(into views/container-body [
-                                                                   (into (views/row-container 0) [[:td.cell] [:td.cell]])
-                                                                   (into (views/row-container 1) [[:td.snake] [:td.snake]])])]))))
-  )
+           (into views/table [[:tbody '([:tr {:key 0} [:td.cell] [:td.cell]]
+                                         [:tr {:key 1} [:td.snake] [:td.snake]])]])))))
