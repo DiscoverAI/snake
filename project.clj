ca931100e216a311fe95c4b5bc86685b1696f929
Daniel Schruhl <danielschruhl@gmail.com>
2018-04-09 18:08:21 +0200

    revert

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (def phantomjs-bin (or (System/getenv "PHANTOMJSBIN")
-                     "./dev-resources/phantomjs"))
+                     "./dev-resources/phantomjs-2.1.1-linux-x86_64"))
 
 (def java-opts (if (= (System/getProperty "java.version") "10") ["--add-modules" "java.xml.bind"] []))
 
