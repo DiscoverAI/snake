95aaf6c906c44c9fd30903c64a9a28cfa72fcca1
Falco Winkler <falcomail95@yahoo.de>
2018-04-20 11:22:01 +0000

    Remove unused change-direction code

--- src/clj/com/github/discoverAI/snake/token.clj
+++
@@ -1,8 +0,0 @@
-(ns com.github.discoverAI.snake.token)
-
-(def LEFT (/ Math/PI 2))
-(def RIGHT (* -1 (/ Math/PI 2)))
-
-(defn change-direction [[x y] angle]
-  [(Math/round (- (* x (Math/cos angle)) (* y (Math/sin angle))))
-   (Math/round (+ (* x (Math/sin angle)) (* y (Math/cos angle))))])
